af8544b17edabe4c6879a878e03adc0b50f3eecc
cdmontez <93397953+cdmontez@users.noreply.github.com>
2021-11-19 12:11:47 -0700

    Prettified

--- ztools/z.js
+++ ztools/z.js
@@ -1,15 +1,17 @@
 /*
-*
-*  z.JS - a JavaScript library by cdmontez
-*  https://github.com/cdmontez
-*
-*/
+ *
+ *  z.JS - a JavaScript library by cdmontez
+ *  https://github.com/cdmontez
+ *
+ */
 
 "use strict";
 var z = {};
 
 z.help = function () {
-  console.log('You can access the zTools documentation here: https://github.com/cdmontez/zTools');
+  console.log(
+    "You can access the zTools documentation here: https://github.com/cdmontez/zTools"
+  );
 };
 
 z.create = function (ELEMENT, CLASSNAME) {
@@ -23,12 +25,12 @@ z.create = function (ELEMENT, CLASSNAME) {
 z.style = function (ELEMENT, STYLES) {
   for (let STYLE in STYLES) {
     ELEMENT.style[STYLE] = STYLES[STYLE];
-  };
+  }
 };
 
 z.codeBlock = function (CLASSNAME) {
-  let pre = document.createElement('pre');
-  let code = document.createElement('code');
+  let pre = document.createElement("pre");
+  let code = document.createElement("code");
   pre.appendChild(code);
   return code;
 };
